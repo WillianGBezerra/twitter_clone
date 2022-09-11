9122f6e5eef64605bd549b4ea44b52a13fa90f85
williangbezerra <williangomesbezerra@gmail.com>
2022-09-11 20:45:01 -0300

    Impedir pesquisar o usuário autenticado.

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -64,6 +64,7 @@
 
 				$usuario = Container::getModel('Usuario');
 				$usuario->__set('nome', $pesquisarPor);
+				$usuario->__set('id', $_SESSION['id']);
 				$usuarios = $usuario->getAll();
 
 				// echo '<pre>';
